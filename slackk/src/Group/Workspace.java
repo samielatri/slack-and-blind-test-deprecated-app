be3f5c7b31d4b35f217c6ee61462bd6c22c8faa6
SinemORUC <sinem.f.oruc@gmail.com>
2020-12-17 08:21:30 +0100

    modifications

--- slackk/src/Group/Workspace.java
+++ slackk/src/Group/Workspace.java
@@ -5,14 +5,16 @@ import UserManagment.User;
 import java.util.ArrayList;
 
 public class Workspace {
-
-
     // UTILISATEURS DU WORKSPACE
+    private String workspaceName;
     private ArrayList<User> workspaceUsers;
-
-
-
     private User admin ;
     private ArrayList<WorkspaceChannel> workspaceWorkspaceChannels;
+
+    public Workspace(String workspaceName, User admin){
+        this.workspaceName = workspaceName;
+        this.admin = admin;
+
+    }
 
 }
