f55b920670b000ecdb8a8ab1a5389ab7a7be7d1d
samielatri <samielatri.pro@gmail.com>
2020-12-17 06:37:31 +0100

    add profile class and attributes

---
+++ slackk/src/Profile.java
@@ -0,0 +1,12 @@
+public class Profile {
+    User user; // user that has the profile
+    Workspace workspace; // the workspace that is related to the user
+
+    String currentStatus; // current status in a specific workspace
+    String completeName; // complete name of the user in a specific workspace
+    String shownName; // shown name in a specific workspace
+    String actualWorkPosition; // actual work position in a specific workspace
+    String phoneNumber; // phone number in a specific workspace
+    String timezone; // timezone in a specific workspace
+    String profilePicture; // profile picture in a specific workspace
+}
