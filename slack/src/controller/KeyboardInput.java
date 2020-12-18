668310feb9dfce900585fbd9d59c53a405fdfcae
samielatri <samielatri.pro@gmail.com>
2020-12-18 11:22:15 +0100

    change name conventions

--- slack/src/controller/KeyboardInput.java
+++ slack/src/controller/KeyboardInput.java
@@ -3,24 +3,25 @@ class ClavierInput {
 
     public void printRead(String printable, int type){
         System.out.println("Please enter " + printable + ": > ");
-        switch (type){
+        switch (type) {
             case 1: // int
                 readInt();
                 break;
             case 2: // String
-                readInt();
+                readString();
                 break;
         }
     }
+
     public int readInt() {
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        return choice;
+        int intInput = scanner.nextInt();
+        return intInput;
     }
 
     public int readString() {
         Scanner scanner = new Scanner(System.in);
-        String string = scanner.nextLine();
-        return string;
+        String stringInput = scanner.nextLine();
+        return stringInput;
     }
 }
