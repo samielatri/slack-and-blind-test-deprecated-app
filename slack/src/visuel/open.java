26f9987d222ff4ac8a0b9e6f6ba6ecb314edc1da
nahidath <vatoch@hotmail.fr>
2020-12-26 21:35:10 -0500

    ajout d'un dossier 'visuel' avec mini interface graphique pour les form

---
+++ slack/src/visuel/open.java
@@ -0,0 +1,8 @@
+package visuel;
+
+public class open {
+    public static void main(String[] args) {
+        slack_visu s= new slack_visu();
+        s.setVisible(true);
+    }
+}
---
+++ slack/visuel/open.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+public class open {
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                /*HomePage h=new HomePage();
+                h.setVisible(true);
+                SignUpPage su=new SignUpPage();
+                su.setVisible(true);*/
+                new HomePage();
+
+            }
+        });
+
+    }
+}
